0e94b83128dd5efaf8c09f970bf5765327b859d5
bensammwaniki <bensammwaniki@gmail.com>
2021-06-23 11:45:11 +0300

    added java script to count no

--- records.js
+++ records.js
@@ -9,3 +9,34 @@ function popup(){
         menu.style.display = "flex"
     }
 }
+
+var count = document.querySelectorAll(".anim");
+var rec = document.querySelector(".rec");
+
+
+
+count.forEach((anim) => {
+  anim.innerHTML = "0";
+var span =["<span>sold</span>","<span>In stock</span>","<span>Recieved</span>","<span>Incoming</span>","<span>sold</span>"]
+var sd = span.forEach(element => {
+  return element;
+});
+  const update = function(){
+    var target = anim.getAttribute('data-target');
+    var no = +anim.innerHTML;
+    var incr =target/300;
+    if(no<target){
+      var fun = function(){  
+      return no+incr;
+      };
+      anim.innerHTML =sd + '</br>' + Math.ceil(fun());
+      setTimeout(update,1)
+    }
+  }
+  update();
+});
+
+
+
+
+rec.style.fontSize = "1.2em";
